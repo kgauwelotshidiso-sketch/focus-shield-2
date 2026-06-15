28cacddfc6c55f35b39f2db08bce1ade13c0b8d3
kgauwelotshidiso-sketch <kgauwelotshidiso@gmail.com>
2026-06-15 19:31:32 +0000

    Upgrade goal system

--- app/sw.js
+++ app/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "focus-shield-v110";
+const CACHE_NAME = "focus-shield-v111";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=backupdata1",
+  "./style.css?v=goalsystem1",
   "./manifest.json",
   "./icon.svg"
 ];
