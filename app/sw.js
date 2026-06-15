c5356dfce445e14620e2156e5cd65c5979453642
kgauwelotshidiso-sketch <kgauwelotshidiso@gmail.com>
2026-06-15 19:07:53 +0000

    Add smart suggestions

--- app/sw.js
+++ app/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "focus-shield-v104";
+const CACHE_NAME = "focus-shield-v106";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=weaknessdetection1",
+  "./style.css?v=smartsuggestionsfix1",
   "./manifest.json",
   "./icon.svg"
 ];
