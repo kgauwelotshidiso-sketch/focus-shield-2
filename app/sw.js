4a9d3c55675e88130afee5a5c069838183d08691
kgauwelotshidiso-sketch <kgauwelotshidiso@gmail.com>
2026-06-16 12:47:08 +0000

    Add end review history

--- app/sw.js
+++ app/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "focus-shield-v133";
+const CACHE_NAME = "focus-shield-v134";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=dailyendreview1",
+  "./style.css?v=endreviewhistory1",
   "./manifest.json",
   "./icon.svg"
 ];
