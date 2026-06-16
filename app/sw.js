900d45980d3e048a6cb0d6b61cfd7aa8322ffed4
kgauwelotshidiso-sketch <kgauwelotshidiso@gmail.com>
2026-06-16 12:32:40 +0000

    Polish quick action menu

--- app/sw.js
+++ app/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "focus-shield-v129";
+const CACHE_NAME = "focus-shield-v131";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=listeningcountfix1",
+  "./style.css?v=hideoldnav1",
   "./manifest.json",
   "./icon.svg"
 ];
