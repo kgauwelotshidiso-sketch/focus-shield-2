5a45d19a81722854c3f1b2d8db90d33ea2573f52
kgauwelotshidiso-sketch <kgauwelotshidiso@gmail.com>
2026-06-18 17:37:22 +0000

    Add feature vault preservation map

--- app/sw.js
+++ app/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "focus-shield-v188";
+const CACHE_NAME = "focus-shield-v189";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=liveappv42concentration1",
+  "./style.css?v=liveappv43featurevault1",
   "./manifest.json",
   "./icon.svg"
 ];
