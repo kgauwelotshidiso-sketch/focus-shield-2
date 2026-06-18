2091ac2e6f4343e60186cf142ef5dc46c8818cbe
kgauwelotshidiso-sketch <kgauwelotshidiso@gmail.com>
2026-06-18 16:30:57 +0000

    Upgrade live action mode v2

--- app/sw.js
+++ app/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "focus-shield-v182";
+const CACHE_NAME = "focus-shield-v183";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=leanliveaction1",
+  "./style.css?v=leanliveactionv2",
   "./manifest.json",
   "./icon.svg"
 ];
